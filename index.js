5e5f8310bf0a218747beacf21f3360a37442993e
Camille Teicheira <cteicheira@gmail.com>
2018-08-08 13:17:49 -0400

    cut down on properties

--- index.js
+++ index.js
@@ -16,11 +16,25 @@ const DOMParser = require('xmldom').DOMParser
 exports.handler = function(event, context, callback) {
 
   request({
-    uri: 'https://inreach.garmin.com/feed/share/'+ INREACHACCT + '?d1=2012-10-16T06:19z',
+    uri: 'https://inreach.garmin.com/feed/share/'+ INREACHACCT + '?d1=2018-06-01T06:19z',
     method: 'GET'
   }, function (err, res){
     const kml = new DOMParser().parseFromString(res.body);
-    const json = togeojson.kml(kml)
+    let json = togeojson.kml(kml)
+
+    json.features = json.features.map(function(r){
+      const s = {}
+      s.type = r.type
+      s.geometry = r.geometry
+      s.properties = {}
+      s.properties['Latitude'] = r.properties['Latitude']
+      s.properties['Longitude'] = r.properties['Longitude']
+      s.properties['Time UTC'] = r.properties['Time UTC']
+      s.properties['Time'] = r.properties['Time']
+      s.properties['timestamp'] = r.properties['timestamp']
+      s.properties['Valid GPS Fix'] = r.properties['Valid GPS Fix']
+      return s
+    })
 
     S3.putObject({
       Body: Buffer.from(JSON.stringify(json, null, 2)),
